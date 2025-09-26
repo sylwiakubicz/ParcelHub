3ee518a55e2f8ebe51e22d28f77292616d10df54
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-09-26 17:10:26 +0200

    Fix connector config.

--- infra/connectors/create-shipment-connector.sh
+++ infra/connectors/create-shipment-connector.sh
@@ -1,29 +1,28 @@
 curl -X POST http://localhost:8083/connectors \
   -H 'Content-Type: application/json' -d '{
-  "name": "pg-outbox",
-  "config": {
     "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
     "plugin.name": "pgoutput",
     "database.hostname": "postgres_db",
     "database.port": "5432",
     "database.user": "postgres",
-    "database.password": "<HASLO>",
+    "database.password": <,
     "database.dbname": "shipment",
     "topic.prefix": "server1",
+
+    "schema.include.list": "shipment",
     "table.include.list": "shipment.outbox_event",
+    "slot.name": "shipment_outbox_slot",
+    "publication.autocreate.mode": "filtered",
 
     "transforms": "outbox",
     "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
     "transforms.outbox.route.by.field": "aggregate_type",
+    "transforms.outbox.route.topic.regex": "(?<r>.*)",
     "transforms.outbox.route.topic.replacement": "${r}-events",
     "transforms.outbox.table.field.event.key": "aggregate_id",
     "transforms.outbox.table.field.payload": "payload",
     "transforms.outbox.table.field.timestamp": "created_at",
-    "transforms.outbox.table.fields.additional.placement": "event_type:header,headers:headers",
+    "transforms.outbox.table.fields.additional.placement": "event_type:header",
 
-    "snapshot.mode": "initial",
-
-    "slot.name": "shipment_outbox_slot",
-    "publication.autocreate.mode": "filtered"
-  }
-}'
+    "snapshot.mode": "initial"
+  }'
