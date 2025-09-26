f4e20a5b5fa504fd12fffdd9bc4b84e64875f2dd
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-09-26 17:11:39 +0200

    Initialise spring project.

---
+++ services/shipment-api/src/main/java/com/parcelHub/shipment_api/ShipmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.parcelHub.shipment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentApiApplication.class, args);
+	}
+
+}
---
+++ services/shipment-api/src/test/java/com/parcelHub/shipment_api/ShipmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcelHub.shipment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
