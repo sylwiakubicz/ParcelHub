4a49ea211064e4ffede3bb34295919d4e50bed84
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-09-27 20:46:25 +0200

    Add http post method to create shipment.

--- services/shipment-api/src/main/java/com/parcelHub/shipment_api/controller/ShipmentController.java
+++ services/shipment-api/src/main/java/com/parcelHub/shipment_api/controller/ShipmentController.java
@@ -1,11 +1,12 @@
 package com.parcelHub.shipment_api.controller;
 
+import com.parcelHub.shipment_api.dto.CreateShipmentRequestDto;
+import com.parcelHub.shipment_api.dto.CreateShipmentResponseDto;
 import com.parcelHub.shipment_api.dto.LabelResponseDto;
 import com.parcelHub.shipment_api.service.ShipmentService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,5 +23,12 @@ public class ShipmentController {
     public ResponseEntity<LabelResponseDto> getShipment(@PathVariable UUID shipmentId) {
         LabelResponseDto labelResponseDto = shipmentService.getShipment(shipmentId);
         return ResponseEntity.ok(labelResponseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateShipmentResponseDto> createShipment(
+            @Valid @RequestBody CreateShipmentRequestDto createShipmentRequestDto) {
+        CreateShipmentResponseDto createShipmentResponseDto = shipmentService.createShipment(createShipmentRequestDto);
+        return ResponseEntity.ok(createShipmentResponseDto);
     }
 }
