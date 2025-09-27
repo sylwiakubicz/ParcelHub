bc9cc91655c5a55be44123767b43d89866c98af9
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-09-27 20:46:10 +0200

    Add service method to createShipment.

--- services/shipment-api/src/main/java/com/parcelHub/shipment_api/service/ShipmentService.java
+++ services/shipment-api/src/main/java/com/parcelHub/shipment_api/service/ShipmentService.java
@@ -1,10 +1,17 @@
 package com.parcelHub.shipment_api.service;
 
+import com.parcelHub.shipment_api.dto.CreateShipmentRequestDto;
+import com.parcelHub.shipment_api.dto.CreateShipmentResponseDto;
 import com.parcelHub.shipment_api.dto.LabelResponseDto;
 import com.parcelHub.shipment_api.exception.ShipmentNotFoundException;
 import com.parcelHub.shipment_api.mapper.ShipmentMapper;
 import com.parcelHub.shipment_api.model.Shipment;
+import com.parcelHub.shipment_api.model.ShipmentStatus;
 import com.parcelHub.shipment_api.repository.ShipmentRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -12,8 +19,13 @@ import java.util.UUID;
 @Service
 public class ShipmentService {
 
+    private final String baseUrl = "https://labels.local";
+
     private final ShipmentRepository shipmentRepository;
     private final ShipmentMapper shipmentMapper;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public ShipmentService(ShipmentRepository shipmentRepository, ShipmentMapper shipmentMapper) {
         this.shipmentRepository = shipmentRepository;
@@ -26,5 +38,45 @@ public class ShipmentService {
        );
         return shipmentMapper.mapShipmentToLabelResponseDto(shipment);
    }
+
+   @Transactional
+   public CreateShipmentResponseDto createShipment(CreateShipmentRequestDto requestDto) {
+        Shipment shipment = shipmentMapper.mapCreateShipmentRequestDtoToShipment(requestDto);
+        shipment.setStatus(ShipmentStatus.CREATED);
+        shipment.setId(UUID.randomUUID());
+
+        Long next = nextLabelSeq();
+        String labelNumber = format(next);
+
+        try {
+            shipment.setLabelNumber(labelNumber);
+            shipment.setLabelUrl(baseUrl + "/" + labelNumber + ".pdf");
+
+            shipment = shipmentRepository.save(shipment);
+
+        } catch (DataIntegrityViolationException e) {
+            if (requestDto.getClientRequestId() != null) {
+                shipment = shipmentRepository.findByClientRequestId(requestDto.getClientRequestId()).orElseThrow();
+            } else {
+                throw e;
+            }
+        }
+
+        // TODO: ZAPIS DO OUTBOXEVENT
+
+        return shipmentMapper.mapShipmentToCreateShipmentResponseDto(shipment);
+   }
+
+    private Long nextLabelSeq() {
+        Object single = entityManager
+                .createNativeQuery("SELECT nextval('shipment.label_number_seq')")
+                .getSingleResult();
+        return ((Number) single).longValue();
+    }
+
+    private String format(Long n) {
+        int year = java.time.Year.now().getValue();
+        return "LBL-%d-%06d".formatted(year, n);
+    }
 
 }
