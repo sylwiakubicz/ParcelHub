12e85b6b235766dd3c8c9bb4e7a45c5e63b3d347
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-10-12 18:30:58 +0200

    Crate first aggregation to KTable and first publication to topic.

--- services/tracking/src/main/java/parcelhub/tracking/tracking/kafka/topology/TrackingTopology.java
+++ services/tracking/src/main/java/parcelhub/tracking/tracking/kafka/topology/TrackingTopology.java
@@ -1,11 +1,23 @@
 package parcelhub.tracking.tracking.kafka.topology;
 
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import parcelhub.tracking.tracking.kafka.props.TopicNames;
+
+import com.parcelhub.shipment.ShipmentCreated;
+import com.parcelhub.shipment.ShipmentKey;
+import com.parcelhub.tracking.Location;
+import com.parcelhub.tracking.LocationType;
+import com.parcelhub.tracking.ShipmentStatus;
+import com.parcelhub.tracking.ShipmentTrackingState;
+
+import java.util.UUID;
 
 @Configuration
 public class TrackingTopology {
@@ -14,12 +26,43 @@ public class TrackingTopology {
     private TopicNames topicNames;
 
     @Bean
-    KStream<Object, Object> trackingSource(StreamsBuilder builder) {
-        // rejestracja zrodla
-        // ten stream subskrybuje topic shipmentEvents,
-        // ktory dziala na domyslnym serde ustawionym w kafka config
-        var stream = builder.stream(topicNames.getShipmentEvents());
-        stream.foreach((k,v) -> {});
+    KStream<ShipmentKey, Object> trackingSource(StreamsBuilder builder) {
+        KStream<ShipmentKey, Object> stream = builder.stream(topicNames.getShipmentEvents());
+
+        stream.foreach((k,v) -> {
+            System.out.println(((SpecificRecordBase) v).getSchema().getFullName());
+        });
+
+        KStream<ShipmentKey, ShipmentTrackingState> states = stream
+                .mapValues((key,value) -> buildStateFromEvent(key, (SpecificRecordBase) value))
+                .filter((k,v) -> v != null);
+
+        KGroupedStream<ShipmentKey, ShipmentTrackingState> grouped = states.groupByKey();
+        KTable<ShipmentKey, ShipmentTrackingState> table = grouped.reduce(
+                (oldVal, newVal) -> newVal,
+                Materialized.<ShipmentKey, ShipmentTrackingState, KeyValueStore<Bytes, byte[]>>as("tracking-state")
+        );
+
+        table.toStream().to(topicNames.getShipmentTracking());
         return stream;
+    }
+
+    static ShipmentTrackingState buildStateFromEvent(ShipmentKey key, SpecificRecordBase value) {
+        if (value instanceof ShipmentCreated sc) {
+            Location none = Location.newBuilder()
+                    .setType(LocationType.NONE)
+                    .setId(null)
+                    .build();
+
+            return ShipmentTrackingState.newBuilder()
+                    .setShipmentId(UUID.fromString(key.getShipmentId().toString()))
+                    .setStatus(ShipmentStatus.CREATED)
+                    .setLastUpdate(sc.getCreatedAt())
+                    .setLastLocation(none)
+                    .setDestinationLockerId(sc.getDestinationLockerId())
+                    .setVersion(1)
+                    .build();
+        }
+        return null;
     }
 }
