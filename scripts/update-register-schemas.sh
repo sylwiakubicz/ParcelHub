1dc85d339e4d24efb535b2f7400da072fc266e68
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-10-17 21:59:38 +0200

    Add script to update connector.

---
+++ scripts/update-register-schemas.sh
@@ -0,0 +1,30 @@
+curl -sS -X PUT http://localhost:8083/connectors/pg-outbox/config \
+  -H 'Content-Type: application/json' -d '{
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "tasks.max": "1",
+
+    "database.hostname": "postgres_db",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "TU_HASLO",
+    "database.dbname": "shipment",
+
+    "topic.prefix": "server1",
+    "schema.include.list": "shipment",
+    "table.include.list": "shipment.outbox_event",
+    "slot.name": "shipment_outbox_slot",
+    "publication.autocreate.mode": "filtered",
+
+    "transforms": "outbox",
+    "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
+    "transforms.outbox.route.by.field": "aggregate_type",
+    "transforms.outbox.route.topic.regex": "(?<r>.*)",
+    "transforms.outbox.route.topic.replacement": "${r}-events",
+    "transforms.outbox.table.field.event.key": "aggregate_id",
+    "transforms.outbox.table.field.payload": "payload",
+    "transforms.outbox.table.field.timestamp": "created_at",
+    "transforms.outbox.table.fields.additional.placement": "event_type:header",
+    "transforms.outbox.table.expand.json.payload": "true",
+
+    "snapshot.mode": "initial"
+  }'
