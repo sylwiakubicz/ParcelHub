7394704e0b1567158b3d59e79091d6de9a344dae
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-10-18 18:38:53 +0200

    Fix config.

--- infra/connectors/update-shipment-connector.sh
+++ infra/connectors/update-shipment-connector.sh
@@ -1,30 +1,31 @@
 curl -sS -X PUT http://localhost:8083/connectors/pg-outbox/config \
   -H 'Content-Type: application/json' -d '{
-    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-    "tasks.max": "1",
+      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+      "plugin.name": "pgoutput",
 
-    "database.hostname": "postgres_db",
-    "database.port": "5432",
-    "database.user": "postgres",
-    "database.password": "TU_HASLO",
-    "database.dbname": "shipment",
+      "database.hostname": "postgres_db",
+      "database.port": "5432",
+      "database.user": "postgres",
+      "database.password": "TU_HASLO",
+      "database.dbname": "shipment",
 
-    "topic.prefix": "server1",
-    "schema.include.list": "shipment",
-    "table.include.list": "shipment.outbox_event",
-    "slot.name": "shipment_outbox_slot",
-    "publication.autocreate.mode": "filtered",
+      "topic.prefix": "server1",
 
-    "transforms": "outbox",
-    "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
-    "transforms.outbox.route.by.field": "aggregate_type",
-    "transforms.outbox.route.topic.regex": "(?<r>.*)",
-    "transforms.outbox.route.topic.replacement": "${r}-events",
-    "transforms.outbox.table.field.event.key": "aggregate_id",
-    "transforms.outbox.table.field.payload": "payload",
-    "transforms.outbox.table.field.timestamp": "created_at",
-    "transforms.outbox.table.fields.additional.placement": "event_type:header",
-    "transforms.outbox.table.expand.json.payload": "true",
+      "schema.include.list": "shipment",
+      "table.include.list": "shipment.outbox_event",
+      "slot.name": "shipment_outbox_slot",
+      "publication.autocreate.mode": "filtered",
 
-    "snapshot.mode": "initial"
+      "transforms": "outbox",
+      "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
+      "transforms.outbox.route.by.field": "aggregate_type",
+      "transforms.outbox.route.topic.regex": "(?<r>.*)",
+      "transforms.outbox.route.topic.replacement": "${r}-events",
+      "transforms.outbox.table.field.event.key": "aggregate_id",
+      "transforms.outbox.table.field.payload": "payload",
+      "transforms.outbox.table.field.timestamp": "created_at",
+      "transforms.outbox.table.fields.additional.placement": "event_type:header",
+      "transforms.outbox.table.expand.json.payload": "true",
+
+      "snapshot.mode": "initial"
   }'
