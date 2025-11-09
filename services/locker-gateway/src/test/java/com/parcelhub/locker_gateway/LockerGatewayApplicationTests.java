5b699219b9f2c5235e77eecb3486ce08c509dcba
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-11-09 20:53:23 +0100

    Project init

---
+++ services/locker-gateway/src/test/java/com/parcelhub/locker_gateway/LockerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcelhub.locker_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
