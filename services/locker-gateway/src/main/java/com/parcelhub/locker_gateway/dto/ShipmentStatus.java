67b2359e7c3e920386e9d8dc2ca1aa4e0b7c916b
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-11-25 21:36:42 +0100

    Add more shipment status to enum

--- services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/dto/ShipmentStatus.java
+++ services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/dto/ShipmentStatus.java
@@ -1,8 +1,19 @@
 package com.parcelhub.locker_gateway.dto;
 
 public enum ShipmentStatus {
+    CREATED,
+    IN_TRANSIT,
+    AT_HUB,
     DROPPED_OFF_AT_LOCKER,
     DELIVERED_TO_LOCKER,
     READY_FOR_PICKUP,
-    PICKED_UP
+    PICKED_UP,
+    RETURN_INITIATED,
+    RETURN_IN_TRANSIT,
+    RETURN_DELIVERED_TO_LOCKER,
+    RETURN_READY_FOR_PICKUP,
+    RETURN_COMPLETED,
+    EXPIRED_AT_LOCKER,
+    CANCELLED,
+    LOST
 }
