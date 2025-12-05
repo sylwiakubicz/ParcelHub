11fdb145b5e452cec22227380241cc59392a0400
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-12-05 20:49:11 +0100

    Send message asynchronously.

--- services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/kafka/publisher/ShipmentEventPublisher.java
+++ services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/kafka/publisher/ShipmentEventPublisher.java
@@ -2,13 +2,19 @@ package com.parcelhub.locker_gateway.kafka.publisher;
 
 import com.parcelhub.locker_gateway.kafka.config.KafkaProperties;
 import org.apache.avro.specific.SpecificRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ShipmentEventPublisher {
     private final KafkaProperties kafkaProperties;
     private final KafkaTemplate<String, SpecificRecord> kafkaTemplate;
+    private Logger logger = LoggerFactory.getLogger(ShipmentEventPublisher.class);
 
     public ShipmentEventPublisher(KafkaProperties kafkaProperties, KafkaTemplate<String, SpecificRecord> kafkaTemplate) {
         this.kafkaProperties = kafkaProperties;
@@ -16,7 +22,15 @@ public class ShipmentEventPublisher {
     }
 
     public void sendMessage(String key, SpecificRecord msg) {
-        // todo oczekiwanie na poprawny response i obsluga bledow
-        kafkaTemplate.send(kafkaProperties.getShipmentEvents(), key, msg);
+        CompletableFuture<SendResult<String, SpecificRecord>> future =
+                kafkaTemplate.send(kafkaProperties.getShipmentEvents(), key, msg);
+        future.whenComplete((r, e) -> {
+            if (e == null) {
+                logger.info("Message sent successfully {}", key);
+            }
+            else {
+                logger.error("Error while sending message {}", key, e);
+            }
+        });
     }
 }
