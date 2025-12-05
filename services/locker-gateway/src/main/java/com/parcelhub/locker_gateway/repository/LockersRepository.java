ea6da8f8f632ade005ab2c612b153f375538aa6c
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-12-05 20:23:07 +0100

    Find locker by shipmentId and lockerId.

--- services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/repository/LockersRepository.java
+++ services/locker-gateway/src/main/java/com/parcelhub/locker_gateway/repository/LockersRepository.java
@@ -4,8 +4,10 @@ import com.parcelhub.locker_gateway.model.Locker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LockersRepository extends JpaRepository<Locker, UUID> {
+    Optional<Locker> findByShipmentIdAndLockerId(UUID shipmentId, String lockerId);
 }
