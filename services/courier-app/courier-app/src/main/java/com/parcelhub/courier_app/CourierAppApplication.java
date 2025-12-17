ac8af9f0289a8d8f9c49f3158b857e8751e2a6f2
sylwiakubicz <sylwiakubicz31@gmail.com>
2025-12-17 14:17:13 +0100

    Initialize courier-app service

---
+++ services/courier-app/courier-app/src/main/java/com/parcelhub/courier_app/CourierAppApplication.java
@@ -0,0 +1,13 @@
+package com.parcelhub.courier_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierAppApplication.class, args);
+	}
+
+}
---
+++ services/courier-app/courier-app/src/test/java/com/parcelhub/courier_app/CourierAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcelhub.courier_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
