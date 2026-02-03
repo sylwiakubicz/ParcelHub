1ce90906d480d622dac565274156cc12068bd46b
sylwiakubicz <sylwiakubicz31@gmail.com>
2026-02-03 17:05:19 +0100

    Initialize sortation service.

---
+++ services/sortation/src/main/java/com/parcelhub/sortation/SortationApplication.java
@@ -0,0 +1,13 @@
+package com.parcelhub.sortation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortationApplication.class, args);
+	}
+
+}
