55f893097e94271053af9a57bfc03653ac6a435c
sylwiakubicz <sylwiakubicz31@gmail.com>
2026-02-15 20:19:13 +0100

    Update ShipmentNotificationState to not have email field.

--- services/notification-service/src/main/java/com/parcelhub/notification_service/kafka/dto/ShipmentNotificationState.java
+++ services/notification-service/src/main/java/com/parcelhub/notification_service/kafka/dto/ShipmentNotificationState.java
@@ -1,5 +1,5 @@
 package com.parcelhub.notification_service.kafka.dto;
 
-public record ShipmentNotificationState(String shipmentId, String recipientPhone, String recipientEmail,
+public record ShipmentNotificationState(String shipmentId, String recipientPhone,
                                         String recipientName, String destinationLockerId) {
 }
