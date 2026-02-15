f27cbfe03310cb2d3a393e3be7c87cacaad4c2d9
sylwiakubicz <sylwiakubicz31@gmail.com>
2026-02-15 20:18:44 +0100

    Create TopologyNames

---
+++ services/notification-service/src/main/java/com/parcelhub/notification_service/kafka/topology/TopologyNames.java
@@ -0,0 +1,5 @@
+package com.parcelhub.notification_service.kafka.topology;
+
+public class TopologyNames {
+    public static final String SHIPMENT_NOTIFICATION_STORE = "shipment-notification-store";
+}
