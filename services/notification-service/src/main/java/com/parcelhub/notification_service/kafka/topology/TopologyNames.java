cf59564e912d5d0e817082444c86684720cdfa5f
sylwiakubicz <sylwiakubicz31@gmail.com>
2026-02-15 20:48:01 +0100

    Add branches names

--- services/notification-service/src/main/java/com/parcelhub/notification_service/kafka/topology/TopologyNames.java
+++ services/notification-service/src/main/java/com/parcelhub/notification_service/kafka/topology/TopologyNames.java
@@ -2,4 +2,7 @@ package com.parcelhub.notification_service.kafka.topology;
 
 public class TopologyNames {
     public static final String SHIPMENT_NOTIFICATION_STORE = "shipment-notification-store";
+
+    public static final String SHIPMENT_NOTIFICATION_BRANCH = "shipment-notification-branch";
+    public static final String READY_FOR_PICKUP_BRANCH = "ready-for-pickup-branch";
 }
